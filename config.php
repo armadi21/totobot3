b882ce237229495e904ee4f10053338826e80c58
armadi21 <45292106+armadi21@users.noreply.github.com>
2018-12-12 00:16:08 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
+//jangan ada sepasi
+$config = [
+
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh3uZuuB5ssC4dtq9eo-linailY-mhpy0hYFkhLC4z393jNGwupKiwKWDzK2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=9424892bc327810c",
+
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh3vbsL2KpLPbitq8jY5ofqyml47MiWK_iIlmho2_2n6KfM2vqnqgwKiC2a2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=9424892bc3449510",
+
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4eXr9CJbb_bd8y9o4avf4a7z4O5eae1eIFnhYq7k393mZiurY6gv9t2lq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=9424892bc489254d",
+
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh57Pur19arSTh8q9fY-ifZ-2lY-mep3AhZNkhLCwzYuHe927rYqks9uLz62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=9424892bc6937bad",
+
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBiojPruCJa7SllNqyenZtf3a23ISpdWTAeIqbhI273YCKg92704Vqv9uClq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=9424892bc6b01e8c",
+
+
+
+
+];
+
+?>
